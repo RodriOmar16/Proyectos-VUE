79929a596b692375dd56d147e504936bf84dbcf9
Rodrigo <rodrigoomarmiranda1@gmail.com>
2024-10-16 22:29:41 -0300

    16102024_R2

--- FundamentosComposition/fundamentos-composition/src/App.vue
+++ FundamentosComposition/fundamentos-composition/src/App.vue
@@ -1,47 +1,43 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <button @click="inc()" >Incrementar</button>
+  <button @click="dec()" >Decrementar</button>
+  <button @click="reset()" >Resetear</button>
+  <button 
+    @click="agregarFav()" 
+    :disabled="array.indexOf(counter) != -1"
+  >
+    Favorito
+  </button>
+  <h1 :style="'color: '+ (counter > 0 ? 'green;' : (counter < 0 ? 'red;' : 'black;'))">
+    {{ counter }}
+  </h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <ul v-for="item in array">
+    <li>{{ item }}</li>
+  </ul>
 
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script setup>
+  //imports
+  import { ref } from 'vue';
+  
+  //Data
+  let counter = ref(0);
+  let array   = ref([]);
+  let pos     = ref(-1);
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  //Methods
+  const inc   = () => counter.value++;
+  const dec   = () => counter.value--;
+  const reset = () => counter.value = 0;
+  function agregarFav(){
+    array.value.push(counter.value);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+</script>
+
+<style>
+
 </style>
